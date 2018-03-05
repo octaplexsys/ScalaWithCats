c92ef1398f5bacf8011eb1e78cf0cd65c7e14383
rmark <rmark@marchex.com>
2018-03-05 15:47:03 -0800

    CatPrinter works

--- src/main/scala/printable/Printable.scala
+++ src/main/scala/printable/Printable.scala
@@ -3,39 +3,38 @@ package printable
 case class Cat(name: String, age: Int, color: String)
 
 trait Printable[A] {
-def format(a: A): String
+  def format(a: A): String
 }
 
 object PrintableInstances{
-  implicit val printableString = {
+  implicit val printableString: Printable[String] = {
     new Printable[String] {
        def format(a: String): String = a
     }
   }
 
-  implicit val printableInt = {
+// NOTE: in Scala 2.12 you can do this!
+//  implicit val printableString: Printable[String] =
+//    (a: String) => a
+
+  implicit val printableInt: Printable[Int] = {
     new Printable[Int] {
       def format(a: Int) = a.toString
     }
   }
 
-  // WHY DOESN'T THIS WORK
-  implicit val catPrinter = {
-    new Printable[Cat] = {
-      def format(a: Cat): String = {
-        s"${a.name} is a ${a.age} year-old ${a.color} cat."
-      }
-    }
-  }
+// NOTE: this also works.
+//  implicit val catPrinter: Printable[Cat] =
+//    (a: Cat) => s"${a.name} is a ${a.age} year-old ${a.color} cat."
 
-  // STILL NOT WORKING
-  implicit def catPrinter[A](implicit printerInt: Printable[Int], printerString: Printable[String]): Printable[Cat] = {
-    new Printable[Cat] = {
-      def format(a: Cat ): String = {
+  implicit def catPrinter(implicit printerInt: Printable[Int], printerString: Printable[String]): Printable[Cat] = {
+    new Printable[Cat] {
+      def format(a: Cat): String = {
         s"${printerString.format(a.name)} is a ${printerInt.format(a.age)} year-old ${printerString.format(a.color)} cat."
       }
     }
   }
+
 }
 
 object Printable {
