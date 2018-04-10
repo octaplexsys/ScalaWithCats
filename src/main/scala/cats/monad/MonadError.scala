24dff9fcd1f23e1c8af1046120c9ecb7a4bcf874
Rebecca Mark <rebecca.lmark@gmail.com>
2018-04-10 16:19:38 -0700

    MonadError notes

--- src/main/scala/cats/monad/MonadError.scala
+++ src/main/scala/cats/monad/MonadError.scala
@@ -10,10 +10,14 @@ class TestErrors {
   def monadError = MonadError[ErrorOr, String]
   val good: Either[String, Long] = monadError.pure(47893L)
   val bad: ErrorOr[Nothing] = monadError.raiseError("It failed")
+  // handleError is like calling recover on a future.
   val handlingError: ErrorOr[ErrorOr[Long]] = monadError.handleError(bad) {
     case "Recoverable" => monadError.pure(123L)
     case "It failed" => monadError.raiseError("Definitely failed")
     case _ => monadError.raiseError("Who knows")
   }
-
+  // ensure is like a filter
+  // you call ensure on a MonadError instance
+  // it's first argument is the monad in question, the second is the fallback, the third is the predicate
+  monadError.ensure(good)("Did not work")((i: Long) => i < 10)
 }
