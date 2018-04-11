c52a7ec41befe324518884b90145ec4dce938676
Rebecca Mark <rebecca.lmark@gmail.com>
2018-04-11 16:53:56 -0700

    EvalMonad has example of eval with memoize

--- src/main/scala/cats/monad/EvalMonad.scala
+++ src/main/scala/cats/monad/EvalMonad.scala
@@ -30,4 +30,23 @@ object EvalMonad extends App {
   println(evalTest.value) // twice
   // Note, that Eval.value is evaluated as a def would be.
 
+  // you can memoize the results of something partway through a chain of things to evaluate
+  val evalWithMemoize: Eval[Int] = Eval.now(0).map {
+    (i) =>
+      println("first step")
+      i + 1
+  }.flatMap { (j) =>
+    println("second step")
+   Eval.always(j + 1)
+  }.memoize
+    .map {(h) =>
+      println("third step")
+      h + 1
+  }
+
+  println
+  println(evalWithMemoize.value)
+  println(evalWithMemoize.value)
+  // This call to get the memoized value of Eval doesn't re-evaluate the memoized portion of the chain
+
 }
