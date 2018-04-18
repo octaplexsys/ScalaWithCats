de08c8a31b13d500bd531f51ed0bd74894790068
Rebecca Mark <rebecca.lmark@gmail.com>
2018-04-17 22:04:43 -0700

    Another version of Writer factorial implemented

--- src/main/scala/cats/monad/WriterFactorial.scala
+++ src/main/scala/cats/monad/WriterFactorial.scala
@@ -2,6 +2,7 @@ package cats.monad
 
 import cats.data.Writer
 import cats.instances.vector._
+import cats.syntax.applicative._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -32,16 +33,29 @@ object WriterFactorial extends App {
     slowly(evalFact)
   }
 
+  def factorial3(n: Int): Logged[Int] = {
+    for {
+      value <- if (n == 0) 1.pure[Logged]
+            else  factorial3(n - 1).map(i => i * n)
+      _ <- Writer.tell(Vector(s"fact $n $value"))
+    } yield value
+  }
+
   Await.result(Future.sequence(Vector(
     Future(factorial(3)),
     Future(factorial(3))
   )), 5.seconds)
 
   println(factorial2(3).run)
+  println(factorial3(3).run)
 
   Await.result(Future.sequence(Vector(
     Future(println(factorial2(4))),
     Future(println(factorial2(4)))
   )), 5.seconds)
 
+  Await.result(Future.sequence(Vector(
+    Future(println(factorial2(4))),
+    Future(println(factorial2(4)))
+  )), 5.seconds)
 }
