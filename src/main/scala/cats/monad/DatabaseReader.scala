4cd84f957193e641e78e2c98f1f3b148be56b038
Rebecca Mark <rebecca.lmark@gmail.com>
2018-04-20 09:57:10 -0700

    CheckLogin implemented in Reader

--- src/main/scala/cats/monad/DatabaseReader.scala
+++ src/main/scala/cats/monad/DatabaseReader.scala
@@ -11,4 +11,20 @@ class RunDatabaseReader {
     Reader(db => db.usernames.get(userId))
   def checkPassword(username: String, password: String): DbReader[Boolean] =
     Reader(db => db.passwords.get(username).contains(password))
+
+  def checkLogin(userId: Int, password: String): DbReader[Boolean] =
+    Reader{db =>
+      val maybeUsername = db.usernames.get(userId)
+      val maybePassword = maybeUsername.flatMap(username => db.passwords.get(username))
+      maybePassword.fold(false)(foundPassword => foundPassword == password)
+    }
+
+  def checkLogin2(userId: Int, password: String): DbReader[Boolean] = {
+    for {
+      maybeUsername <- findUsername(userId)
+      bool <- for {
+        username <- maybeUsername
+      } yield checkPassword(username, password)
+    } yield bool
+  }
 }
