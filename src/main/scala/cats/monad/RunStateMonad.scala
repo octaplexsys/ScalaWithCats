8d86171922bae5cdc00832d64fa2c635600a9df2
Rebecca Mark <rebecca.lmark@gmail.com>
2018-04-27 15:24:14 -0700

    StateMonad example methods

--- src/main/scala/cats/monad/RunStateMonad.scala
+++ src/main/scala/cats/monad/RunStateMonad.scala
@@ -1,20 +1,41 @@
 package cats.monad
 
-import cats.data.State
+import cats.Eval
+import cats.data.{IndexedStateT, State}
 
 object RunStateMonad extends App {
-  val s: State[Int, String] = State{(stateInt: Int) => (stateInt + 1, s"The state is $stateInt") }
+  val s: State[Int, String] = State {(stateInt: Int) =>
+    val result = stateInt + 1
+    (result, s"The state is $result")
+  }
 
   // Note: Run returns an Eval for stack safety, so we need to call value to extract it.
-  val (state, string) = s.run(10).value
+  val (resultState, string) = s.run(10).value
   val stateWithoutValue = s.runA(10).value
   val valueWithoutState = s.runS(10).value
 
   println(stateWithoutValue)
   println(valueWithoutState)
 
-  val step1 = State{(stateString: String) => (stateString + "!", 1)}
-  val step2 = step1.map(i => i + 1)
-  println(step2.run("statefulString").value)
+  val stateString = State{ (stateString: String) => (stateString + "!", 1)}
+  val stateString2 = stateString.map(i => i + 1)
+  println(stateString2.run("statefulString").value)
+
+  val state1 = State[Int, String] {(int: Int) =>
+    val ans = int + 1
+    (ans, s"result of step1: $ans")
+  }
+  val state2 = State[Int, String] {(int: Int) =>
+    val ans = int * 2
+    (ans, s"result of step2: $ans")
+  }
+
+  val both: IndexedStateT[Eval, Int, Int, (String, String)] = for {
+    one <- state1
+    two <- state2
+  } yield (one, two) // Hmmm what is this doing? Not a tuple the way I would have expected
+
+  val (state, result) = both.run(42).value
+  println(s"state: $state, result: $result")
 
 }
