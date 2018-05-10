ed84c80fdbf3edb968ae1775efc9fc421fcc9d5a
Rebecca Mark <rebecca.lmark@gmail.com>
2018-05-10 15:45:55 -0700

    treemonad WIP pure and flatMap implemented

--- src/main/scala/cats/functor/Tree.scala
+++ src/main/scala/cats/functor/Tree.scala
@@ -1,6 +1,6 @@
 package cats.functor
 
-import cats.Functor
+import cats.{Functor, Monad}
 
 sealed trait Tree[+T]
 final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
@@ -14,6 +14,17 @@ object Tree{
         case Branch(left, right) => Branch(map(left)(me),map(right)(me))
       }
     }
+  }
+
+  val treeMonad = new Monad[Tree] {
+    override def pure[A](x: A): Tree[A] = Leaf(x)
+
+    override def flatMap[A, B](fa: Tree[A])(f: A => Tree[B]): Tree[B] = fa match {
+      case Branch(left, right) => Branch(flatMap(left)(f), flatMap(right)(f))
+      case Leaf(value) => f(value)
+    }
+
+    override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] = ???
   }
 }
 
