938b86d91176aa317073edd0322b4c4c6bbfb0ed
Rebecca Mark <rebecca.lmark@gmail.com>
2018-05-10 15:29:32 -0700

    statecalc testing work

--- src/test/scala/monad/CalculatorTest.scala
+++ src/test/scala/monad/CalculatorTest.scala
@@ -14,5 +14,8 @@ class CalculatorTest extends WordSpec with Matchers {
     "take in two numbers and an operator and perform the evaluation" in {
       evalAll("1,2,+").run(List()) shouldBe (List(3), ())
     }
+    "take in multiple numbers and operators" in {
+      evalAll("0,2,*,1,+,5,*").run(List()) shouldBe (List(5),())
+    }
   }
 }
