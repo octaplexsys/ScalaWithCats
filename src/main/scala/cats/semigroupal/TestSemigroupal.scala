5bd51c6daa2ee63e72e139bb0cf716ed8792c4c6
rmark <rmark@marchex.com>
2018-07-12 16:30:20 -0700

    Cat Monoid Semigroupal work

--- src/main/scala/cats/semigroupal/TestSemigroupal.scala
+++ src/main/scala/cats/semigroupal/TestSemigroupal.scala
@@ -4,6 +4,7 @@ import cats.instances.option._
 
 
 case class Cat(name: String, age: Int, color: String)
+case class Cat2(name: String, yearOfBirth: Int, favoriteFoods: List[String])
 
 object TestSemigroupal extends App {
   val sgOpt1 = Semigroupal[Option].product(Some(213), Some("ABC"))
@@ -26,6 +27,19 @@ object TestSemigroupal extends App {
   val optionalcat = (Option("Garfield"), Option(1213), Option("Orange")).mapN{Cat}
   println(optionalcat)
 
+  import cats.Monoid
+  import cats.instances.boolean._
+  import cats.instances.int._
+  import cats.instances.list._
+  import cats.instances.string._
 
+  val tupleToCat: (String, Int, List[String]) => Cat2 = Cat2.apply _
+  val catToTuple: Cat2 => (String, Int, List[String]) = cat => (cat.name, cat.yearOfBirth, cat.favoriteFoods)
+
+  implicit val catMonoid: Monoid[Cat2] = (
+    Monoid[String],
+    Monoid[Int],
+    Monoid[List[String]]
+  ).imapN(tupleToCat)(catToTuple) // WTF is this doing Ask Greg / Matt
 
 }
